ca9e0ecee6fbdf4b289926ec8969a2daba0f9e4e
Rahim <ragim95@gmail.com>
2016-04-19 08:11:43 +0400

    Added controls and view

--- app/src/main/java/com/tutsplus/matt/bluetoothscanner/ControlActivity.java
+++ app/src/main/java/com/tutsplus/matt/bluetoothscanner/ControlActivity.java
@@ -3,27 +3,31 @@ package com.tutsplus.matt.bluetoothscanner;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.tutsplus.matt.bluetoothscanner.ConnectThread;
 import com.tutsplus.matt.bluetoothscanner.Connecting.ManageConnectThread;
 
 import java.io.IOException;
 import java.util.UUID;
 
+import lejos.remote.ev3.RemoteRequestEV3;
+
 public class ControlActivity extends AppCompatActivity implements View.OnTouchListener {
     private BluetoothDevice device;
     private BluetoothSocket socket;
     private ConnectThread connectThread;
+    private RemoteRequestEV3 ev3;
     private ManageConnectThread thread;
-    private UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    private UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private Button up, down, leftUp, leftDown, rightUp, rightDown, connectBT;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +42,6 @@ public class ControlActivity extends AppCompatActivity implements View.OnTouchLi
         leftDown = (Button) findViewById(R.id.button_left_down);
         rightUp = (Button) findViewById(R.id.button_right_up);
         rightDown = (Button) findViewById(R.id.button_right_down);
-
         up.setOnTouchListener(this);
         down.setOnTouchListener(this);
 
@@ -51,12 +54,17 @@ public class ControlActivity extends AppCompatActivity implements View.OnTouchLi
         connectBT = (Button) findViewById(R.id.button_connect);
 
         Log.d("CONTROL", device.getName());
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
     }
 
     public void onConnectClick(View view) {
         connectThread = new ConnectThread(device, mUUID);
         if (connectThread.connect()) {
             Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
+            connectBT.setBackgroundColor(getResources().getColor(R.color.green));
+            connectBT.setTextColor(getResources().getColor(R.color.white));
         }
         socket = connectThread.getbTSocket();
         thread = new ManageConnectThread();
@@ -96,17 +104,14 @@ public class ControlActivity extends AppCompatActivity implements View.OnTouchLi
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else if (event.getAction() == MotionEvent.ACTION_UP) {
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
             try {
-                thread.sendData(socket,'s');
+                thread.sendData(socket, 's');
             } catch (IOException e) {
                 e.printStackTrace();
             }
             Log.d("clicked", "stop");
         }
-
-
         return false;
     }
 }
